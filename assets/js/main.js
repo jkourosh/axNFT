bfe92a45d7d381b8f2bb0dbc067eb7c6fc9474ca
jkourosh <jkourosh@gmail.com>
2022-03-08 01:58:10 +0330

    stg1

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+// Grab elements
+const selectElement = (selector) => {
+	const element = document.querySelector(selector);
+	if (element) return element;
+	throw new Error(`Error Selector: ${selector}`);
+};
+
+const scrollHeader = () => {
+	const headerElement = selectElement('#header');
+	if (this.scrollY >= 15) {
+		headerElement.classList.add('activated');
+	} else {
+		headerElement.classList.remove('activated');
+	}
+};
+
+//Nav styles on scroll
+window.addEventListener('scroll', scrollHeader);
+
+
+// Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+if (currentTheme) {
+	bodyElement.classList.add('light-theme');
+}
+themeToggleBtn.addEventListener('click', () => {
+	bodyElement.classList.toggle('light-theme');
+	if (bodyElement.classList.contains('light-theme')) {
+		localStorage.setItem('currentTheme', 'themeActive');
+	} else {
+		localStorage.removeItem('currentTheme');
+	}
+});
+
+
+
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchformcontainer = selectElement('#search-form-container');
+formOpenBtn.addEventListener('click', () => searchformcontainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchformcontainer.classList.remove('activated'));
+
+// -- Close the search form popup on ESC keypress
+window.addEventListener('keyup', (e) => {
+	if (e.key === 'Escape') searchformcontainer.classList.remove('activated');
+});
+
+
+
+
+// Swiper
